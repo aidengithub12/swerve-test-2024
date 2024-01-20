a2ef4e946b3e6c33fd6dfbc35b1317ed8c20f89e
aidengithub12 <147771123+aidengithub12@users.noreply.github.com>
2024-01-20 13:04:17 -0600

    Update ShooterIOSparkMax.java

--- src/main/java/frc/robot/subsystems/Shooter/ShooterIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIOSparkMax.java
@@ -15,7 +15,6 @@ package frc.robot.subsystems.Shooter;
 
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import frc.robot.subsystems.*;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 import com.revrobotics.CANSparkMax;
